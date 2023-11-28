1ad0637a84c1dac94d3f2f1ec670f8eae8f2fabe
Niharika <97402437+niharika1102@users.noreply.github.com>
2023-11-28 20:51:07 +0530

    Update employee.js

--- routes/employee.js
+++ routes/employee.js
@@ -1,24 +1,12 @@
-// const express = require('express')
-// const router = express.Router()
-
-// const EmployeeController = require('../controllers/EmployeeController')
-
-// router.get('/', EmployeeController.index)
-// router.post('/show', EmployeeController.show)
-// router.post('/store', EmployeeController.store)
-// router.post('/update', EmployeeController.update)
-// router.post('/delete', EmployeeController.destroy)
-
-// module.exports = router
-
 const express = require('express')
 const router = express.Router()
 
-const studentController = require('../controllers/EmployeeController')
-router.get('/',studentController.index)
-router.get('/getData',studentController.getData)
-router.get('/createData',studentController.createData)
-router.get('/updateData',studentController.updateData)
-router.get('/delete',studentController.deleteData)
+const EmployeeController = require('../controllers/EmployeeController')
+
+router.get('/', EmployeeController.index)
+router.post('/show', EmployeeController.show)
+router.post('/store', EmployeeController.store)
+router.post('/update', EmployeeController.update)
+router.post('/delete', EmployeeController.destroy)
 
 module.exports = router
