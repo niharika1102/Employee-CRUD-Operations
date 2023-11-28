ebdb20a4943797980aa99dbe3ab002352f255e8e
Niharika <97402437+niharika1102@users.noreply.github.com>
2023-11-28 20:51:29 +0530

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -1,44 +1,23 @@
-// const mongoose = require('mongoose')
-// const Schema = mongoose.Schema
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-// const employeeSchema = new Schema ({
-//     name: {
-//         type: String
-//     },
-//     designation: {
-//         type: String
-//     },
-//     email: {
-//         type: String
-//     },
-//     phone: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// }, {timestamps: true})
-
-// const Employee = mongoose.model('Employee', employeeSchema)
-// module.exports = Employee.
-
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const studentSchema = new Schema({
-    name:{
+const employeeSchema = new Schema ({
+    name: {
         type: String
     },
-    Batch:{
-        type: Number
-    },
-    age:{
-        type: Number
-    },
-    email:{
+    designation: {
         type: String
+    },
+    email: {
+        type: String
+    },
+    phone: {
+        type: String
+    },
+    age: {
+        type: Number
     }
-} , {timestamps: true})
+}, {timestamps: true})
 
-const Student = mongoose.model('Student',studentSchema);
-module.exports = Student;
+const Employee = mongoose.model('Employee', employeeSchema)
+module.exports = Employee
